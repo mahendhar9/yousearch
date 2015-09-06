734c89f41f7dd4fb89b8a964615cd8b1cefa3beb
Mahendhar <mahendarreddy@live.com>
2015-09-07 00:50:24 +0530

    Added prev-page and next-page buttons

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $('#input-search').focus(function(){
     $(this).animate({
       width: '63%'
-    },500);
+    },300);
   });
 
   // Blur
@@ -12,7 +12,7 @@ $(document).ready(function(){
     if ($(this).val() == '') {
       $(this).animate({
         width: '30%'
-      },500);
+      },300);
     }
   });
 
@@ -23,23 +23,32 @@ $(document).ready(function(){
 
 function search() {
 
-  //Empty the search results before searching again.
+  //Empty the search results and buttons before searching again.
   $('#results').html('');
+  $('#buttons').html('');
 
   //Get the query in search bar
-  q = $('#input-search').val();
+  query = $('#input-search').val();
 
   $.get("https://www.googleapis.com/youtube/v3/search",{
     part: 'snippet,id',
-    q: q,
-    type: 'video',    
+    q: query,
+    type: 'video',
+    maxResults: 10,
     key: 'AIzaSyBpIlH2SCdrlyn_eEWAkJcrdxDScNxUdwY'}, function(data){
       console.log(data);
+
+      var nextPageToken = data.nextPageToken;
+      var prevPageToken = data.prevPageToken;
+
+
       $.each(data.items, function(i, item){
         var output = getOutput(item);
-
         $('#results').append(output);
       });
+
+      var buttons = getButtons(prevPageToken, nextPageToken);
+      $('#buttons').append(buttons);
     });
   }
 
@@ -60,7 +69,82 @@ function search() {
             '<p class="channel-meta">By<i> '+channelTitle+'</i> on '+publishedAt+'</p>'+
             '<p>'+description+'</p>' +
             '</div>' + 
-            '</div>'
+            '</div>' +
+            '<hr>'
 
     return getoutput;       
+  }
+
+function getButtons(prevPageToken, nextPageToken){
+  if (!prevPageToken){
+    var pageButtons = '<button id="next-button" class="btn btn-default" data-token="'+nextPageToken+'" data-query="'+query+'" onclick="nextPage();">Next Page</button>'
+  }
+  else {
+    var pageButtons = '<button id="prev-button" class="btn btn-default" data-token="'+prevPageToken+'" data-query="'+query+'" onclick="prevPage();">Prev Page</button>' +
+    '<button id="next-button" class="btn btn-default" data-token="'+nextPageToken+'" data-query="'+query+'" onclick="nextPage();">Next Page</button>'
+  }
+
+  return pageButtons;
+}
+
+function nextPage(){
+  var token = $('#next-button').data('token'); 
+  var query = $('#next-button').data('query');
+ 
+  $('#results').html('');
+  $('#buttons').html('');
+
+  $.get("https://www.googleapis.com/youtube/v3/search",{
+    part: 'snippet,id',
+    q: query,
+    pageToken: token,
+    type: 'video',
+    maxResults: 10,  
+    key: 'AIzaSyBpIlH2SCdrlyn_eEWAkJcrdxDScNxUdwY'}, function(data){
+      console.log(data);
+
+      var nextPageToken = data.nextPageToken;
+      var prevPageToken = data.prevPageToken;
+
+
+      $.each(data.items, function(i, item){
+        var output = getOutput(item);
+        $('#results').append(output);
+      });
+
+      var buttons = getButtons(prevPageToken, nextPageToken);
+      $('#buttons').append(buttons);
+    });
+  
+  }
+
+function prevPage(){
+  var token = $('#prev-button').data('token'); 
+  var query = $('#prev-button').data('query');
+ 
+  $('#results').html('');
+  $('#buttons').html('');
+
+  $.get("https://www.googleapis.com/youtube/v3/search",{
+    part: 'snippet,id',
+    q: query,
+    pageToken: token,
+    type: 'video',
+    maxResults: 10, 
+    key: 'AIzaSyBpIlH2SCdrlyn_eEWAkJcrdxDScNxUdwY'}, function(data){
+      console.log(data);
+
+      var nextPageToken = data.nextPageToken;
+      var prevPageToken = data.prevPageToken;
+
+
+      $.each(data.items, function(i, item){
+        var output = getOutput(item);
+        $('#results').append(output);
+      });
+
+      var buttons = getButtons(prevPageToken, nextPageToken);
+      $('#buttons').append(buttons);
+    });
+  
   }
